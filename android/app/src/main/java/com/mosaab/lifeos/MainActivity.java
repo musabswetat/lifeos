a8c2ecb4adae6a1636a4134adccb359e79088f87
musabswetat <musabsw1@gmail.com>
2026-08-28 23:15:04 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/mosaab/lifeos/MainActivity.java
+++ android/app/src/main/java/com/mosaab/lifeos/MainActivity.java
@@ -1,11 +1,5 @@
 package com.mosaab.lifeos;
 
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
-
-package com.yourcompany.lifeos; // تأكد من اسم الحزمة الخاص بك
-
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
@@ -22,7 +16,7 @@ public class MainActivity extends BridgeActivity {
     private void createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             CharSequence name = "LifeOS Alarms";
-            String description = "قناة إشعارات وتنبيهات Life OS المباشرة";
+            String description = "قناة إشعارات وتنبيهات Life OS";
             int importance = NotificationManager.IMPORTANCE_HIGH;
             NotificationChannel channel = new NotificationChannel("lifeos_alarms_channel", name, importance);
             channel.setDescription(description);
